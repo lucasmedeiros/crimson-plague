80337b74ec0b8274b4eed33d2c29f407d82d922f
MarcellaAlmeida <marcella.almeida@ccc.ufcg.edu.br>
2018-09-26 22:59:38 -0300

    corrigindo erros

--- cpp/monstros/monstros.cpp
+++ cpp/monstros/monstros.cpp
@@ -8,10 +8,10 @@ Monstro getMonstro(string* informacoes) {
 	monstro.id = stoi(informacoes[0].c_str());
 	monstro.nome = informacoes[1];
 	string* infoDado = split(informacoes[2],'d', 2);
-	monstro.numDados = stoi(infoDado[0].c_str());
+	monstro.numDado = stoi(infoDado[0].c_str());
 	monstro.tipoDado = stoi(infoDado[1].c_str());
-	monstro.divisor = stoi(informacoes[3].c_str());
-	monstro.somador = stoi(informacoes[4].c_str());
+	monstro.divisorDado = stoi(informacoes[3].c_str());
+	monstro.somadorDado = stoi(informacoes[4].c_str());
 	monstro.xp = stoi(informacoes[5].c_str());
 	monstro.drop = stoi(informacoes[6].c_str());
 	monstro.ca = stoi(informacoes[7].c_str());
@@ -19,11 +19,11 @@ Monstro getMonstro(string* informacoes) {
 	return monstro;
 }
 
-Monstros* carregarAtributos() {
-	string** atributos = importarTodos("../db/atrMonstros.txt",string caminho, QTD_COLUNAS, QTD_LINHAS_MONTROS, QTD_COMENTARIOS);
-	Monstro* monstros = new Monstro[QTD_LINHAS_MONSTROS];
-	for(int i = 0; i < QTD_LINHAS_MONSTROS; i++)
-		monstros[i] = getMonstro(linhas[i]);
+Monstro* carregarAtributos() {
+	string** atributos = importarTodos("../db/atrMonstros.txt",QTD_COLUNAS, QTD_LINHAS, QTD_COMENTARIOS);
+	Monstro* monstros = new Monstro[QTD_LINHAS];
+	for(int i = 0; i < QTD_LINHAS; i++)
+		monstros[i] = getMonstro(atributos[i]);
 	return monstros;
 };
 
--- cpp/monstros/monstros.h
+++ cpp/monstros/monstros.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../util.h"
 #define QTD_COMENTARIOS 2
-#define QTD_LINHAS_MONSTROS 7
+#define QTD_LINHAS 7
 #define QTD_COLUNAS 9
 
 struct Monstro {
