7dd60418f8b3e4df15171125d93b171aaff02e0a
Luan <jadsonluan1@gmail.com>
2018-10-16 11:23:11 -0300

    Adiciona função prompt

--- hs/Util.hs
+++ hs/Util.hs
@@ -1,15 +1,27 @@
 module Util (
   getOption,
-  clearScreen
+  clearScreen,
+  prompt
 ) where
 
 import qualified System.Process
 
+-- Dado um titulo, apresenta o titulo e aguarda uma resposta
+-- Por fim, retorna a resposta
+prompt :: String -> IO String
+prompt text = do
+  putStr text
+  response <- getLine
+  return response
+
+-- Limpa a tela
 clearScreen :: IO()
 clearScreen = do
   _ <- System.Process.system "clear"
   return ()
 
+-- Mostra uma mensagem pedindo para inserir uma opção,
+-- e em seguida, retorna essa opção
 getOption :: IO Int
 getOption = do
   putStr "Informe o número da opção desejada: "
