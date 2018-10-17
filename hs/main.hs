dd35f7b93706c1ae36ab04d1ca925bc414baeb56
Luan <jadsonluan1@gmail.com>
2018-10-16 23:10:02 -0300

    Corrige nome do metodo que carrega habilidades

--- hs/main.hs
+++ hs/main.hs
@@ -4,5 +4,5 @@ import qualified CharInfo.Spell as Spells
 main = do
   character <- Sheet.createCharacter
   -- Inicializa lista de habilidades
-  spells <- Spells.loadSpells
+  spells <- Spells.loadAll
   return character
