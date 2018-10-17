0726b263fe10a75c6619086a73b72d745f30f5cb
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2018-10-17 20:51:52 -0300

    Adicao de funcao para carregar todos os itens a partir do bd

--- hs/Itens/Item.hs
+++ hs/Itens/Item.hs
@@ -10,17 +10,51 @@ data Item = AtrbItens(
     str :: Int,
     int :: Int,
     dex :: Int,
-    vit :: Int
+    vit :: Int,
+    class :: String
 ) deriving (Show)
 
 data Item = Item(
-    id :: Int
-    name :: String
-    description :: String
-    sell :: Int
-    consumable :: Bool
-    recHPMax :: Int
-    recMPMax :: Int
+    id :: Int,
+    name :: String,
+    description :: String,
+    sell :: Int,
+    consumable :: Bool,
+    recHPMax :: Int,
+    recMPMax :: Int,
+    atrb :: AtrbItens
 ) deriving (Show)
+
+generateAtributes :: [String] -> AtrbItens
+generateAtributes txt = do
+    let dam = read (txt !!1) :: Int
+    let   arm = read (txt !!2) :: Int
+    let   str = read (txt !!3) :: Int
+    let   vit = read (txt !!4) :: Int
+    let   dex = read (txt !!5) :: Int
+    let   int = read (txt !!6) :: Int
+    
+    AtrbItens dam arm str int dex vit
+
+BuildItem :: [String] -> [String] -> Item
+BuildItem txt atrbtxt =  do
+    let   id = read (txt !!0) :: Int
+    let   name = txt !!1 :: String
+    let   description = txt !!2 :: String
+    let   sell = read (txt !!6) :: Int
+    let   consumable = txt !!3 :: Bool
+    let   recHPMax = read (txt !!4) :: Int
+    let   recMPMax = read (txt !!5) :: Int
+    let   atrb = generateAtributes atrbtxt :: AtrbItens
+    
+    Item id name description sell consumable recHPMax recMPMax atrb
+
+loadAllItens :: IO [Item]
+loadAllItens = do
+    itensTxt <- Database.importFromDB "../db/itens_db.txt" 3
+    atrbTxt <- Database.importFromDB "../db/atributos_itens_db.txt" 4
+    return $ map BuildItem itensTxt atrbTxt
+
+
 
 
