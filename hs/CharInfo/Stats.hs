4b5ce9c9aba6d8ee373f7aa1014c0541b41941f1
Jadson Luan <13488025+jadsonluan@users.noreply.github.com>
2018-10-23 00:33:56 -0300

    Implementa métodos de modificação de HP e MP

--- hs/CharInfo/Stats.hs
+++ hs/CharInfo/Stats.hs
@@ -1,6 +1,8 @@
 module CharInfo.Stats (
   Stats(..),
   fillStats,
+  increaseMP,
+  increaseHP,
   addXP
 ) where
 
@@ -19,6 +21,12 @@ data Stats = Stats {
   xp :: Int,
   level :: Int
 } deriving (Show)
+
+increaseMP :: Int -> Stats -> Stats
+increaseMP num stats = Stats (hp stats) (max_hp stats) ((mp stats) + num) (max_mp stats) (xp stats) (level stats)
+
+increaseHP :: Int -> Stats -> Stats
+increaseHP num stats = Stats ((hp stats) + num) (max_hp stats) (mp stats) (max_mp stats) (xp stats) (level stats)
 
 addLevel :: Int -> Stats -> Stats
 addLevel n stats = Stats (hp stats) (max_hp stats) (mp stats) (max_mp stats) (xp stats) ((level stats) + n)
