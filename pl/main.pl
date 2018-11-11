694dbd32fd9ac29f1a406c1291d16548a01fb4c6
LukeHxH <lucasmed812@gmail.com>
2018-11-10 22:48:49 -0300

    implementa loop de escolha de role

--- pl/main.pl
+++ pl/main.pl
@@ -1,18 +1,24 @@
-%! Define main como a regra principal do programa
 :- initialization(main).
 
-%! Fatos
 classe("1", "guerreiro").
 classe("2", "mago").
 classe("3", "ladino").
 
-			
-main:-
+escolheClasse() :-
+    writeln("0) Informações"),
+    writeln("1) Guerreiro"),
+    writeln("2) Mago"),
+    writeln("3) Ladino"),
+    writeln("Digite sua classe:"),
+    read_line_to_string(user_input, Opcao),
+    avaliaOpcaoClasse(Opcao, Output),
+    writeln(Output).
 
-writeln("1) Guerreiro"),
-writeln("2) Mago"),
-writeln("3) Ladino"),
-writeln("Digite sua classe:"),
-read_line_to_string(user_input, Opcao),
-classe(Opcao, Classe),
-writeln(Classe).
+avaliaOpcaoClasse(Value, Output) :-
+    classe(Value, Output).
+avaliaOpcaoClasse(Value, Output) :-
+    writeln("Opção inválida!"),
+    escolheClasse().
+
+main:-
+    escolheClasse().
