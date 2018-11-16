82e3f94c93d0021aaa89bf96d6517684fe5c5b43
jessesouza21 <jesse.neto@ccc.ufcg.edu.br>
2018-11-16 08:06:53 -0300

    Progresso no inventário e limpeza de código

--- pl/Itens/Inventory.pl
+++ pl/Itens/Inventory.pl
@@ -2,7 +2,7 @@ setup_Itens:-
 	consult('itens.pl').
 
 
-% --------------------- FACTS ----------------------------
+% --------------------- LISTS AND GETS ---------------------
 :- dynamic(equipped/1).
 equipped([34,34,34,34,34]).
 
@@ -42,8 +42,7 @@ addItem(Id):-
 	asserta(bag(Y)),
 	addQtdItem(Id).
 
-
-% Remove item in Bag (CHECKED)
+% Remove item da b0ag (CHECKED)
 
 removeItem(Id):-
 	getBag(X),
@@ -53,7 +52,7 @@ removeItem(Id):-
 	Qtd > 1 -> removeNoSoloItem(Id);
 	removeSoloItem(Id).  	
 
-% Consume item in bag and return Mp and Hp
+% Retorna atributos do item consumivel (MP E HP)
 
 consumeItem(Id,MP,HP):-
 	removeItem(Id),
@@ -61,58 +60,66 @@ consumeItem(Id,MP,HP):-
 	getAtrbConsumable(Id,MP,HP).
 
 
-% ------------------- GET ATRIBUTES (ALL CHECKED) ---------------------
+% ------------------- GET SUM ATRIBUTES (ALL CHECKED) ---------------------
 
-% Sum damage of the equipped itens 
+% Retorna o dano total dos itens equipados 
 
 sumDamage(Dam):-
 	equipped(X),
 	sumDAM(X,Dam).
 
-% Sum armor of the equipped itens
+% Retorna a armadura total dos itens equipados
 
 sumArmor(Arm):-
 	equipped(X),
 	sumARM(X,Arm).
 
-% Sum streigth of the equipped itens
+% Retorna a força total dos itens equipados
 
 sumStreigth(Str):-
 	equipped(X),
 	sumSTR(X,Str).
 
-% Sum inteligence of the equipped itens
+% Retorna a inteligência total dos itens equipados
 
 sumInteligence(Int):-
 	equipped(X),
 	sumINT(X,Int).
 
-% Sum agility of the equipped itens
+% Retorna a agilidade total dos itens equipados
 
 sumAgility(Dex):-
 	equipped(X),
 	sumDEX(X,Dex).
 
+% -------------------------- UTIL PREDICATES ------------------------------
 
-% ------------------ AUX PREDICATES ------------------------
+
+% Troca um item pelo outro na lista (o primeiro no caso de existir mais de um)
 
 replaceItem(_,_,[],[]):- !.
 replaceItem(NewValue,OldValue,[OldValue|T],[NewValue|T]).
 replaceItem(NewValue,OldValue,[H|T],[H|T1]):- replaceItem(NewValue,OldValue,T,T1).
 
+% Encontra o index de um valor da lista
 % CHECK
 findIndexByValue(E,[E|_],0):- !.
 findIndexByValue(E,[_|T],I):- findIndexByValue(E,T,X), I is X + 1.
 
+% Encontra o valor pelo index na lista
 % CHECK
 findValueByIndex(0, [H|_], H):- !.
 findValueByIndex(I, [_|T], E):- X is I - 1, findValueByIndex(X, T, E).
 
+% substitui um valor no index especifico dentro de uma lista, retornando a mesma atualizada
 replace(_,_,[],_,_).
 replace(V,0,[_|T],E,U):- append(E,[V],Y), append(Y,T,U).
 replace(V,I,[X|T],E,U):- In is I - 1,append(E,[X],Y),replace(V,In,T,Y,U).
 
 
+% -------------------------- AUX PREDICATES ------------------------------
+
+% Função aux para remover quando possuir apenas um item (qtd = 1)
 removeSoloItem(Id):-
 	getQtd(Y),
 	getBag(X),
@@ -128,7 +135,7 @@ removeSoloItem(Id):-
 	asserta(bag(NewBag)),
 	asserta(qtd(NewQtd)).
 
-
+% Função aux para remover quando possuir mais de um item (qtd > 1)
 removeNoSoloItem(Id):-
 	getQtd(Y),
 	getBag(X),
@@ -141,7 +148,7 @@ removeNoSoloItem(Id):-
 	replace(Nq,Index,Y,[],NewQtd),
 	asserta(qtd(NewQtd)).
 
-
+% Função aux para adicionar a quantidade de um item 
 addQtdItem(Id):-
 	retract(qtd(Y)),
 	getBag(X),
@@ -154,31 +161,35 @@ addQtdItem(Id):-
 	asserta(qtd(NewQtd)).
 
 
-
+% Função aux para somar o Dano total
 sumDAM([],0).
 sumDAM([X|T],Y):-
 	 setup_Itens,
 	 getDamage(X,Dam),
 	 sumDAM(T,Z), Y is Z+Dam. 
 
+% Função aux para somar a força total
 sumSTR([],0).
 sumSTR([X|T],Y):-
 	 setup_Itens,
 	 getStrength(X,STR),
 	 sumSTR(T,Z), Y is Z+STR. 
 
+% Função aux para somar a inteligência total
 sumINT([],0).
 sumINT([X|T],Y):-
 	 setup_Itens,
 	 getIntelig(X,INT),
 	 sumINT(T,Z), Y is Z+INT. 
 
+% Função aux para somar o a agilidade total
 sumDEX([],0).
 sumDEX([X|T],Y):-
 	 setup_Itens,
 	 getAgility(X,DEX),
 	 sumDEX(T,Z), Y is Z+DEX.
 
+% Função aux para somar o armadura total
 sumARM([],0).
 sumARM([X|T],Y):-
 	setup_Itens,
