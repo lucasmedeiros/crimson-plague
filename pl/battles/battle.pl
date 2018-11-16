c0da3622cbad26338c2cc1b6fd7f670cdb7ccd73
LukeHxH <lucasmed812@gmail.com>
2018-11-15 23:34:20 -0300

    adiciona ataque de personagem e início da implementação de ataque mosntro

--- pl/battles/battle.pl
+++ pl/battles/battle.pl
@@ -1,3 +1,7 @@
+rollDice(O) :-
+    random(0, 21, RollResult),
+    O = RollResult.
+
 loopBattle(M, C) :- 
     atom_concat("HP Monstro: ", M, MonsterHP),
     atom_concat("Seu HP: ", C, CharHP),
@@ -13,23 +17,35 @@ menu(M, C, M2, C2) :-
     writeln("1) Atacar/Lançar magia"),
     writeln("3) Fugir"),
     read_line_to_string(user_input, Option),
-    (evaluateOption(Option, M, C, M2, C2); menu(M, C, M2, C2)).
+    evaluateOption(Option, M, C, M2, C2).
 
 evaluateOption(O, M, C, M2, C2) :-
-    O = "1" -> 
-        charAttack(M, C, M2, C2);
-    O = "3" ->
-        tryEscape(M, C, M2, C2);
-    (writeln("Opção inválida!"), loopBattle(M2, C2)).
+    O = "1" -> charAttack(M, C, M2, C2);
+    O = "3" -> tryEscape(M, C, M2, C2);
+    (writeln("Opção inválida!"), menu(M, C, M2, C2)).
 
 charAttack(M, C, M2, C2) :-
-    random(1, 21, RollResult),
+    rollDice(RollResult),
     M2 is (M - RollResult),
-    C2 is C + 0.
+    C2 is C,
+    monsterAttack(M, C, M2, C2).
+
+monsterAttack(M, C, M2, C2) :-
+    rollDice(RollResult),
+    CharDef is 20,
+    MonsterDMG is 10 + RollResult,
+    writeln("O monstro se prepara para um ataque..."),
+    MonsterDMG >= CharDef ->
+        (atom_concat("E inflige um total de", MonsterDMG, Concat1),
+        atom_concat(Concat1, " danos em você...", Concat2),
+        writeln(Concat2),
+        C2 is C - MonsterDMG,
+        loopBattle(M2, C2));
+    (writeln("E falha miseravelmente..."), loopBattle(M2, C)).
 
 tryEscape(M, C, M2, C2) :-
     writeln("Você tenta fugir e..."),
-    random(0, 21, RollResult),
+    rollDice(RollResult),
     RollResult >= 12 -> 
         writeln("Escapou com sucesso...");
-    (writeln("Não conseguiu... O monstro ri de você"), loopBattle(M2, C2)).
+    (writeln("Não conseguiu... O monstro ri de você"), monsterAttack(M, C, M2, C2)).
