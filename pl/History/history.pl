113759498cc5a33e8dc142bb89f164a19338bb79
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2018-11-16 23:15:03 -0300

    Todas as falas da versao em C passadas, faltando a logica da historia em si

--- pl/History/history.pl
+++ pl/History/history.pl
@@ -187,18 +187,6 @@ segundaEscolhaEntrada:-
               "nada relevante pra voce.",
               "Você acende uma tocha e entra na caverna."].
 
-printCredits:-
-    Creditos = ["Obrigado por jogar: A Praga Carmesim!",
-                "Sistema inspirado: Dungeons and Dragons, quinta edição.",
-                "",
-                "Feito por:",
-                "Jadson Luan",
-                "Jesse Souza",
-                "Lucas de Medeiros",
-                "Mikael Brasileiro",
-                "",
-                "Existem mais finais para serem desbravados, tente conseguir outro jogando novamente!"].
-
 cavernReception:-
     Dial = ["O tunel se abre em uma camara pequena e aproximadamente",
             "regular. Pedacos espalhados de minerio de prata cercam",
@@ -206,13 +194,18 @@ cavernReception:-
             "aparenta ser um corpo humano se sobressai debaixo de uma",
             "das carrocas. E nao mostra nenhum sinal de movimento.",
             "Sangue seco mancha a parede norte em varios locais.",
-            "Há uma saída para o oeste"
+            "Há uma saída para o oeste",
             ""].
 
 receptionChoice:-
     Opcao = ["O que você vai fazer? (digite um número)",
             "1) Analisar o corpo da carroca.",
             "2) Seguir em frente."].
+
+receptionConclusion:-
+    Dial = ["Apos passar da carroca, voce so ve uma grande porta a sua frente.",
+    "Voce nao ve outra opcao a nao ser entrar nela."].
+
 
 checkWagon1:-
     Opcao1 = ["Voce escuta um barulho de",
@@ -232,6 +225,7 @@ checkWagon2:-
 checkWagon3:-
     Dial = ["Opção invalida!"].
 
+
 wagonDescription:-
     Dial = ["Ao analisar o corpo, voce percebe que ele tem marcas parecidos com",
             "as vitimas da praga. Ele tem uma roupa diferente dos outros mineradores",
@@ -245,6 +239,29 @@ getSilver:-
     Dial1 = ["Voce coleta os minerios sem problemas."],
 
     Dial2 = ["Voce prefere nao arricar a pegar esses minerios."].
+
+
+koboldsCombatConversation:-
+    Opcao1 = ["Ao comecar a falar, os kobolds apesar de receiosos",
+            "resolvem escutar o que voce tem a dizer",
+            "O que você irá dizer para eles?",
+            "1) Nao precisamos brigar. Nenhum de nos quer isso! (Diplomacia)",
+            "2) Nao ousem me atacar se quiserem continuar vivos. (Intimidacao)"].
+
+koboldsCombatDialogue2:-
+    DialOp1 = ["Os kobolds parecem concordar com voce e abaixam as armas.",
+            "O que parece ser o capitao daquele pequeno grupo fala:",
+            "Capitão: Nao temos forca pra lutar, doenca enfraquecer e matar",
+            "Capitão amigos. Humano pode seguir em frente."],
+
+    DialOp2 = ["Os kobolds parecem, de fato, ficarem intimidados com voce.",
+               "O que parece o capitao daquele pequeno grupo fala:",
+               "Capitão: Tudo bem, humano pode passar. Só não nos mate, por favor."],
+
+    DialOp3 = ["Os kobolds aceitam sua proposta, provavelmente por falta de",
+                    "força ou coragem de lutar.","Voce entrega o dinheiro para eles e segue seu caminho."],
+
+    DialOp4 = ["Opção invalida!"].
 
 checkListenKnowledge:-
     Opcao1 = ["Voce consegue escutar varios passos do outro lado da porta",
@@ -280,28 +297,189 @@ auxCheckKnowledge:-
               "nada. No entanto, você percebe que os kobolds estão nervosos",
               "pelo tom de voz."].
 
-koboldsCombatConversation:-
-    Opcao = ["Ao comecar a falar, os kobolds apesar de receiosos",
-            "resolvem escutar o que voce tem a dizer",
-            "O que você irá dizer para eles?",
-            "1) Nao precisamos brigar. Nenhum de nos quer isso! (Diplomacia)",
-            "2) Nao ousem me atacar se quiserem continuar vivos. (Intimidacao)"].
-
-koboldsCombatDialogue2:-
-    DialOp1 = ["Os kobolds parecem concordar com voce e abaixam as armas.",
-            "O que parece ser o capitao daquele pequeno grupo fala:",
-            "Capitão: Nao temos forca pra lutar, doenca enfraquecer e matar",
-            "Capitão amigos. Humano pode seguir em frente."],
-
-    DialOp2 = ["Os kobolds parecem, de fato, ficarem intimidados com voce.",
-               "O que parece o capitao daquele pequeno grupo fala:",
-               "Capitão: Tudo bem, humano pode passar. Só não nos mate, por favor."],
-
-    DialOp3 = ["Opção invalida!"].
-
 startBattleKobolds:-
     Dial = ["O que parece ser o capitao daquele pequeno grupo fala:"
             "Capitao: Nao importa o que humano fale, voce morre agora!"].
+
+refectoryCavern:-
+    Opcao = ["Ter ativado a armadilha atraiu muito a atencao daqueles que",
+            "estao dentro da caverna, inclusive os kobolds dentro dessa sala.",
+            "Voce escuta muitos gritos e passos, voce imagina que eles estao",
+            "se preparando para um combate. Ao abrir a porta você ve o seguinte:"],
+
+    Dial = ["Uma sala larga e quadrada abriga quatro longas mesas de",
+            "madeira, cada uma com um banco de cada lado. Em cima",
+            "das mesas estao um numero de tigelas de madeira e talheres.",
+            "No canto sudeste da sala, um pequeno caldeirao fumega",
+            "sobre um fogao cravado no chao. Um cheiro pungente esta",
+            "suspenso no ar. ",
+            "Voce também exerga três kobolds apontando para voce.",
+            ""].
+
+refectoryChoice:-
+    Opcoes = ["Os kobolds parecem bastante nervosos, o que voce ira fazer?",
+            "1) Tentar conversar com eles.",
+            "2) Atacar imediatamente."].
+
+refectoryChoice2:-
+    Dial = ["Voce imediatamente comeca a preparar o seu ataque enquanto",
+            "os kobolds correm em direcao a voce."],
+
+    Invalid = ["Opção invalida!"].
+
+pantryCavern:-
+    Dial1 = ["Uma porta de madeira fechada barra a entrada para essa",
+            "sala. Ao abrir porta você observa que esta sala longa",
+            "e estreita possui fileiras de estantes carregadas",
+            "de alimentos e outros suprimentos. Pesados sacos",
+            "de estopa estão empilhados até o teto no canto",
+            "oposto da câmara. Uns poucos rasgados, espalhando",
+            "aveia e farinha no chão. Muitos barris estão",
+            "agrupados próximos à parede norte.",
+            "Ao entrar na sala, uma armadilha é ativada!",
+            "Uma flecha é lançada em sua direção."],
+
+    Opcao11 = ["Por sorte, a armadilha não te acerta."],
+
+    Opcao12 = ["A flecha perfura o seu ombro. Causando",
+              "um ferimento moderado."],
+
+    Dial2 = ["A armadilha chama a atenção de uma criatura",
+            "que estava em um barril e ela irá atacar você.",
+            "Essas criatura é um rato, no entanto, bem maior",
+            "que o normal, e você tem a impressão que pode pegar",
+            "a praga lutando com ela."],
+
+    Opcao21 = ["Apesar do contato com os ratos, você consegue",
+              "resistir a praga."],
+
+    Opcao22 = ["Após o combate, você sente que algo está errado...",
+               "Você está se sentindo um pouco mais fraco, talvez",
+               "você tenha contraído a praga."],
+
+    Dial3 = ["Em um dos barris, você encontra duas poções de vida",
+            "e duas poções de mana, naturalmente você guarda elas.",
+            "Elas podem ser bastante úteis no futuro."],
+
+    Dial4 = ["Após os achados, você começa a descer uma rampa que",
+            "leva a uma parte inferior da caverna. Aos poucos ",
+            "ela vai ficando muito íngrime, a um ponto que te",
+            "faz perder o equilíbrio e descer o resto da rampa",
+            "deslizando a mesma."].
+
+corpsesGrave:-
+    Dial1 = ["Você passa pelo túnel. Ondas de calor banham",
+            "respiração difícil. Esta caverna pequena e",
+            "em forma de tigela possui o chão cheio de",
+            "cadáveres humanóides. A abertura de um túnel",
+            "é visível na parede leste da câmara",
+            "e, na distância, o som de água corrente",
+            "pode ser ouvido."],
+
+    Opcao1 = ["Você acredita que aqui é o fosso da criação",
+              "da praga. Sejá la quem for que está fazendo",
+              "isso, não deve ficar longe de onde você está."],
+
+    Opcao2 = ["Quatro zumbis levantam-se de um dos montes",
+              "de corpos! Prepare-se para o combate!"],
+
+    Dial2 = ["Algo dentro de você diz que isso está perto",
+            "de acabar. Ao calmo som de água corrente. Você",
+            "entra no estreito túnel a leste."].
+
+
+rampCavern:-
+    Dial1 = ["Após descer a rampa e chegar no fundo da caverna",
+            "você olha que esta caverna enorme se estende",
+            "para cima até pelo menos 30 m, subindo alto para",
+            "dentro das entranhas da montanha. A iluminação",
+            "ocasionalmente reflete pequenos grãos prateados,",
+            "que brilham na face parede oeste da caverna.",
+            "Muitos cabos estão pendurados na beirada de um",
+            "largo vazio na parede. O buraco penetra a face",
+            "oeste da caverna e começa a cerca de 6 m de",
+            "onde você está. O ar aqui é mais frio e úmido.",
+            "De repente, você escuta vários passos. Não de",
+            "humanoides, mas de um animal. Um imenso",
+            "lobo aparece por trás de algumas pedras e",
+            "começa a te cercar, e ele vai te atacar!"],
+
+    Dial2 = ["",
+            "Após o combate, você percebe que pode escalar",
+            "aqueles cabos pendurados. No entanto, também",
+            "existe um túnel à frente"].
+
+rampChoice:-
+    Choices = ["O que voce vai fazer?",
+               "1) Escalar os cabos",
+               "2) Seguir em frente"].
+
+rampProgress1:-
+    Opcao1 = ["Aṕos escalar os cabos, você sobe em uma",
+              "pedra e exerga uma passagem para o que",
+              "parece um acampamento, você consegue",
+              "exergar que existem muitos tesouros",
+              "lá dentro."],
+
+    Opcao2 = ["Você escala os cabos, mas devido ao escuro,",
+              "você não consegue enxergar nada. Sua única",
+              "opção é voltar e seguir o outro caminho."].
+
+rampProgress2:-
+    Dial = ["Você prefere não se arriscar escalando esses",
+            "cabos. É mais sensato manter o foco e seguir",
+            "o caminho."].
+
+rampProgress3:-
+    Dial = ["Opção inválida!"].
+
+secretCamp:-
+    Dial = ["Ao entrar na passagem, você encontra o acampa-",
+            "mento dos Kobolds, usado como área de estoque",
+            "e dormitório. Você encontra um baú e dois",
+            "jovens Kobolds, que estão olhando para você",
+            "e estão amedrontados. Um deles utiliza um capuz",
+            "que parece valioso."].
+
+secretCampChoice:-
+    Dial = ["O que voce ira fazer?",
+            "Matar eles e ficar com a capa.",
+            "Poupar a vida deles e pegar apenas o tesouro."].
+
+secretCampDecision1:-
+    Dial = ["Para você, a capa é mais valiosa que a vida",
+            "deles. Você os mata sem remorso e pega a",
+            "capa deles, com um pouco de investigação",
+            "você descobre que é uma capa da proteção."].
+
+secretCampDecision2:-
+    Dial = ["Você poupa a vida deles, afinal, eles não",
+            "fizeram absolutamente nada. Simplesmente não",
+            "seria justo. Você pega três poções de vida",
+            "e duas de mana. Logo após, você desce os cabos",
+            "E segue o outro caminho."].
+
+secretCampDecision3:-
+    Dial = ["Opção inválida!"].
+
+situacionGettingWorst:-
+    Dial = ["A leve fraqueza que você estava sentindo",
+    "está demasiadamente pior, você já sente","um pouco de dificuldade em movimentos",
+    "bruscos ou que precisam de força. No entanto,","você continua determinado a acabar com esse","problema."].
+
+grave of the corpses:-
+    Dial1 = ["Você passa pelo túnel. Ondas de calor banham",
+    "respiração difícil. Esta caverna pequena e","em forma de tigela possui o chão cheio de",
+    "cadáveres humanóides. A abertura de um túnel","é visível na parede leste da câmara",
+    "e, na distância, o som de água corrente","pode ser ouvido."],
+
+    Dial2 = ["Você acredita que aqui é o fosso da criação",
+        "da praga. Sejá la quem for que está fazendo","isso, não deve ficar longe de onde você está."],
+    
+    Dial3 = ["Quatro zumbis levantam-se de um dos montes","de corpos! Prepare-se para o combate!"],
+
+    Dial4 = ["","Algo dentro de você diz que isso está perto","de acabar. Ao calmo som de água corrente. Você",
+    "entra no estreito túnel a leste."].
 
 jakkEnding:-
     L = ["Ao entrar, nessa parte da caverna, você observa um",
@@ -327,6 +505,56 @@ jakkEnding:-
         "clava de aço na mão direita.",
         "O Orc prepara sua maça enquanto você corre",
         "em sua direção."].
+
+jakkConversation:-
+    Dial1 = ["Jakk: Meu nome é Jakk, o que voce quer? Voce",
+    "Jakk: se arricou muito para chegar aqui.","Jakk: Que tolice."],
+
+    Dial2 = ["O que voce sabe sobre a praga?","Eu tenho a cura, me mate e voce nunca a obtera.(Enganacao)",
+    "Nao importa o que voce sabe, eu vim aqui para lutar com voce"],
+
+    Opcao1 = ["Jakk: Eu que criei e estou matendo essa praga.",
+        "Jakk: Eu enfeiticei a agua para causar essa doenca.","Jakk: Portanto, cuspa logo o que voce quer ou lute comigo!"],
+
+    Opcao2 = ["Jakk: Pare de mentir, humano insolente!","Jakk: Eu que criei e estou mantendo essa praga.",
+        "Jakk: Eu enfeiticei a agua para causar essa doenca.","Jakk: Entao cuspa logo o que voce quer ou lute comigo!"],
+
+    Opcao3 = ["Jakk: Voce nao tem uma chance, seu verme."],
+
+    Dial3 = ["Por que esta fazendo isso?","Entao eu tenho o dever de para-lo."],
+
+    Dial4 = ["Jakk: 10 anos atras, o povo de Passagem de Duvik quase",
+        "Jakk: exterminou o meu clã, o clã Garra Despedaçante, pois","Jakk: achavam que nós éramos uma ameaça. Eles não quiseram",
+        "Jakk: ter nenhuma forma de conversa. Essa é a vinganca","Jakk: do meu clã."],
+
+    Dial5 = ["Jakk: É hora de morrer, seu verme insolente!"],
+
+    Dial6 = ["Matando eles voce fica tao errado quanto aquelesque mataram o seu cla. Voce nao combate odio com odio.",
+    " Voce esta matando ainda mais pessoas do seu cla. Voce nao viu a quantidade de kobolds mortos? Pare com isso,todos os seus soldados querem sair daqui e esquecer","Isso nao justifica os seus erros. Voce morrera agora!"],
+
+    Dial7 = ["Jakk: O que voce sabe sobre meu cla? Voce nao sabe o que",
+        "Jakk: esta falando... Voce nao sabe o quanto meu cla sofreu.","Jakk: Pare de falar sobre o que você não tem ideia."],
+
+    Opcao3 = ["Jakk parece um pouco relutante.","Jakk: Eu sei, eu sou o que mais sofro com isso. No entanto,",
+        "Jakk: todos eles concordaram com isso, eu tenho que valorizar","Jakk: as mortes daqueles que se sacrificaram pela causa!"],
+
+    Opcao4 = ["Jakk: Voce nao tem uma chance, seu verme."],
+
+    Interpretou = ["Jakk, esqueca isso... Voce salvara a vida de muitas Pessoas do seu cla, desistindo disso.",
+        "Seja um pouco racional... Isso nao trara alegria a ninguem.","Eu tenho certeza que a vontade de Gruumsh eh que isso acabe pacificamente."],
+
+    NInterpretou = ["Jakk, esqueca isso... Voce salvara a vida de muitas Pessoas do seu cla, desistindo disso.",
+        "Seja um pouco racional... Isso nao trara alegria a ninguem."],
+
+    SolucaoJakk1 = ["Jakk: É verdade... Meu povo está sofrendo demais com isso.",
+        "Jakk: Eu vou quebrar o feitico imediatamente. Obrigado por","Jakk: trazer razao ao meu pensamento, humano."],
+
+    SolucaoJakk2 = ["Jakk: Voce tem toda a razao... O que eu estava pensando?",
+        "Jakk: Eu vou quebrar o feitico imediatamente. Obrigado","Jakk: por trazer sensatez, humano."],
+
+    SolucaoJakk3 = ["Jakk: Voce parece sensato, mas tenho que honrar meu clã",
+        "Jakk: É hora de lutar, humano."].
+
 
 printViolentEnding:-
     Dial1 = ["Extremamente abatido, Jakk fala baixo:",
@@ -402,3 +630,15 @@ printVariantEnding_Else:-
             "Meruen: forma de agradadecimento. Por fim, volte",
             "Meruen: aqui de vez em quando. Seria muito bom",
             "Meruen: te ver novamente!"].
+
+printCredits:-
+    Creditos = ["Obrigado por jogar: A Praga Carmesim!",
+                "Sistema inspirado: Dungeons and Dragons, quinta edição.",
+                "",
+                "Feito por:",
+                "Jadson Luan",
+                "Jesse Souza",
+                "Lucas de Medeiros",
+                "Mikael Brasileiro",
+                "",
+                "Existem mais finais para serem desbravados, tente conseguir outro jogando novamente!"].
