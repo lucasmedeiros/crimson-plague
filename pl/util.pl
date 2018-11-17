66966fe713a55bc2edabcd7e77b1eef6834b1ec8
LukeHxH <lucasmed812@gmail.com>
2018-11-17 19:08:17 -0300

    getters para os monstros + adição de rolagem de mais de 1 dado em util

--- pl/util.pl
+++ pl/util.pl
@@ -1,9 +1,21 @@
 :- module(util,
-    [rollDice/2, cls/0, printList/1]).
+    [rollDice/2, rollDices/3, cls/0, printList/1]).
 
-rollDice(L, O) :-
-    random(1, L, RollResult),
+rollDice(Bound, O) :-
+    random(1, Bound, RollResult),
     O = RollResult.
+
+rollDices(N, Bound, O) :-
+    rollDices(N, Bound, 0, O).
+
+rollDices(0, _, O, O).
+rollDices(N, Bound, X, O) :-
+    N > 0,
+    rollDice(Bound, RollResult),
+    X1 is X+RollResult,
+    N1 is N-1,
+    rollDices(N1, Bound, X1, O).
+
 
 printList([]) :-
     write("").
