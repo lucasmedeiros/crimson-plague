a2a61ac350bd66aeb4cfa7fbf99c4ff5b78918b9
Luan <jadsonluan1@gmail.com>
2018-11-17 23:17:52 -0300

    Adiciona inicialização do jogo

--- pl/main.pl
+++ pl/main.pl
@@ -1,25 +1,6 @@
 :- initialization(main).
 
-classe("1", "guerreiro").
-classe("2", "mago").
-classe("3", "ladino").
-classe("0", ).
-
-escolheClasse() :-
-    writeln("0) Informações"),
-    writeln("1) Guerreiro"),
-    writeln("2) Mago"),
-    writeln("3) Ladino"),
-    writeln("Digite sua classe:"),
-    read_line_to_string(user_input, Opcao),
-    avaliaOpcaoClasse(Opcao, Output),
-    writeln(Output).
-
-avaliaOpcaoClasse(Value, Output) :-
-    classe(Value, Output).
-avaliaOpcaoClasse(Value, Output) :-
-    writeln("Opção inválida!"),
-    escolheClasse().
+:- use_module("Sheet/sheet").
 
 main:-
-    escolheClasse().
+    sheet:createCharacter.
