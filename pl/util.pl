e32d153c809720c979c34dbc62b09aa93c23e451
Jesse Souza <jesse.neto@ccc.ufcg.edu.br>
2018-11-22 13:45:01 -0300

    correcao do module de util

--- pl/util.pl
+++ pl/util.pl
@@ -1,5 +1,5 @@
 :- module(util,
-    [rollDice/2, rollDices/3, cls/0, printList/1, readInt/1, readString/1, getYesNo/1,printInterface/1]).
+    [rollDice/2, rollDices/3, cls/0, printList/1, readInt/1, readString/1, getYesNo/1]).
 
 rollDice(Bound, O) :-
     random(1, Bound, RollResult),
